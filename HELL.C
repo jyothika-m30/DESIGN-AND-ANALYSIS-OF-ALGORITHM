6a8204ad46eb4a823d6b1192d0efa0198908c8c2
jyothika-m30 <mjyothika3010@gmail.com>
2025-11-10 09:14:50 +0530

    hello.c

---
+++ HELL.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    for(int i = 0; i < 10; i++)
+        printf("Hello\n");
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nExecution Time: %f seconds\n", cpu_time_used);
+    return 0;
+}
